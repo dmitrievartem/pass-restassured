a9618de486620500e02c412fcce7fe484b904124
Артем Дмитриев <artem2733@yandex.ru>
2021-07-31 15:14:51 +0300

    add tags

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/features/",
         glue = {"steps", "hooks"},
-        tags = {"@SmokeTest"},
+        tags = {"@all"},
         snippets = SnippetType.CAMELCASE
 )
 
